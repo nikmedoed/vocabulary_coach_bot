bb6b00e7dc4f1731c06962a9ac3f96617167f63c
Nikita <grafniksan@gmail.com>
2023-02-18 01:10:47 +0600

    database update checker fix

--- telegram/utils/database_update.py
+++ telegram/utils/database_update.py
@@ -4,7 +4,7 @@ from .config_types import Config
 from .aiogram_redis_ext import RedisStorage2ext
 from telegram.utils.constants import StorageKeys
 from telegram.texts import Text
-
+import logging
 from telegram.utils.spreadsheet_connector import SpreadSheetConnector
 
 DATABASE_VERSION = '1.1'
@@ -26,5 +26,9 @@ async def user_database_update(bot: Bot):
     config: Config = bot.get('config')
     storage: RedisStorage2ext = bot["storage"]
     users = await storage.get_all_users_values()
-    tasks = [check_user_base_version(bot, u, v.get(StorageKeys.SHEET_URL.name)) for u, v in users.items()]
-    await asyncio.gather(*tasks)
+
+    for u, v in users.items():
+        try:
+            await check_user_base_version(bot, u, v.get(StorageKeys.SHEET_URL.name))
+        except Exception as e:
+            logging.error(f"PreviosMessageMiddleware - {e.__class__}: {e}")
