447e308d0e722f54a75339f1b37769d4a3ba552d
Nikita <grafniksan@gmail.com>
2023-12-26 19:36:33 +0400

    redis fixes

--- telegram/utils/aiogram_redis_ext.py
+++ telegram/utils/aiogram_redis_ext.py
@@ -10,7 +10,7 @@ class RedisStorage2ext(RedisStorage2):
                       default: typing.Optional[dict] = None) -> typing.Optional[typing.Union[str, int, None]]:
         chat, user = self.check_address(chat=chat, user=user)
         key = self.generate_key(chat, user, str(key))
-        redis = await self._get_adapter()
+        redis = self._redis
         raw_result = await redis.get(key)
         return raw_result or default
 
@@ -18,7 +18,7 @@ class RedisStorage2ext(RedisStorage2):
                       user: typing.Union[str, int, None] = None):
         chat, user = self.check_address(chat=chat, user=user)
         key = self.generate_key(chat, user, str(key))
-        redis = await self._get_adapter()
+        redis = self._redis
         if value:
             await redis.set(key, str(value), ex=self._bucket_ttl)
         else:
@@ -26,7 +26,7 @@ class RedisStorage2ext(RedisStorage2):
 
     async def get_all_users_keys(self, key="*"):
         key = self.generate_key("*", "*", str(key))
-        redis = await self._get_adapter()
+        redis = self._redis
         return await redis.keys(key)
 
     async def get_all_users(self, key="*"):
@@ -36,7 +36,7 @@ class RedisStorage2ext(RedisStorage2):
     async def get_all_users_values(self, key="*") -> dict:
         keys = await self.get_all_users_keys(key)
         user_values = defaultdict(dict)
-        redis = await self._get_adapter()
+        redis = self._redis
         for key in keys:
             user, value_key = key.split(":")[-2:]
             value = await redis.get(key)
